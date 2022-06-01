a211117e16d8fdec3d06afad0661df2bfb0b8a63
Duc Le <Duc.Le@tallygroup.com.au>
2022-06-02 00:53:30 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.ebf24014c532ccd2b46d902976bba1c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "81d9cef756fc2a13dbc6",
+    "url": "/static/js/main.69c77b93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87dd27e0c1148b650bb1",
+    "url": "/static/js/2.ef3ac334.chunk.js"
+  },
+  {
+    "revision": "de5d438eac96f3158a39288de966e0ff",
+    "url": "/index.html"
+  }
+];
